139921a592db6d37d1e63b5b6c862aec58949a87
mahuanfy <mahuanfy@foxmail.com>
2018-06-09 12:48:22 +0800

    完成生命游戏简单逻辑功能

---
+++ src/main/java/com/thoughtworks/logic/MainFuction.java
@@ -0,0 +1,74 @@
+package com.thoughtworks.logic;
+
+public class MainFuction {
+    private int n = 10;
+    public int[][] matrix = new int[n][n];
+    public int[][] nextmatrix = new int[n][n];
+
+    public void setN(int n) {
+        this.n = n;
+    }
+
+    public void initMatrix() {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = (int) Math.round(Math.random());
+            }
+        }
+    }
+
+    public void setMatrix() {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = nextmatrix[i][j];
+                nextmatrix[i][j] = 0;
+            }
+        }
+    }
+
+    public int getNum(int x, int y, int matrix[][]) {
+        int num = 0;
+        int d[][] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}, {1, 1}, {1, -1}, {-1, -1}, {-1, 1}};
+        for (int i = 0; i < 8; i++) {
+            int xx = x + d[i][0];
+            int yy = y + d[i][1];
+            if (xx >= 0 && xx < n && yy >= 0 && yy < n && matrix[xx][yy] != 0) {
+                num++;
+            }
+        }
+        return num;
+    }
+
+    public void changeState(int x, int y) {
+        int neighbour = getNum(x, y, matrix);
+        if (neighbour == 3) {
+            nextmatrix[x][y] = matrix[x][y] + 1;
+        } else if (neighbour == 2) {
+            if (matrix[x][y] > 0) {
+                nextmatrix[x][y] = matrix[x][y] + 1;
+            }
+        } else {
+            nextmatrix[x][y] = 0;
+        }
+    }
+
+    public void check() {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                changeState(i, j);
+            }
+        }
+    }
+
+    public void printWhole() {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+
+}
---
+++ src/test/java/com/thoughtworks/logic/MainFuctionTest.java
@@ -0,0 +1,66 @@
+package com.thoughtworks.logic;
+
+import org.junit.Assert;
+import org.junit.Test;
+public class MainFuctionTest {
+
+    MainFuction mainFuction = new MainFuction();
+
+
+    @Test
+    public void setN() {
+    }
+
+    @Test
+    public void initMatrix() {
+        mainFuction.initMatrix();
+    }
+
+    @Test
+    public void setMatrix() {
+        mainFuction.setMatrix();
+    }
+
+    @Test
+    public void get_num_where_x_1_y_1() {
+        int [][] n= {{1, 0, 0}, {0, 1, 0}, {1, 1, 1}};
+        int num = mainFuction.getNum(1, 1,n);
+        Assert.assertEquals(4,num);
+    }
+    @Test
+    public void get_num_where_x_2_y_2() {
+        int [][] n= {{1, 0, 0},
+                     {0, 1, 0},
+                     {1, 1, 1}};
+        int num = mainFuction.getNum(2, 2,n);
+        Assert.assertEquals(2,num);
+    }
+
+    @Test
+    public void changeState() {
+    }
+
+    @Test(timeout = 1000)
+    public void check() {
+        mainFuction.check();
+    }
+
+    @Test
+    public void printWhole() {
+        mainFuction.printWhole();
+
+    }
+
+    @Test(timeout = 5000)
+    public void main() throws InterruptedException {
+        initMatrix();
+        printWhole();
+        while (true) {
+            check();
+            setMatrix();
+            printWhole();
+            Thread.sleep(1000);
+        }
+
+    }
+}
